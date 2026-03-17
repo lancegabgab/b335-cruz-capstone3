68a84ac21ef6536ca33dbc0e8fda1d645ea0a42b
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-17 08:57:46 +0800

    Update UserView.js
    rendered OrderStatusTabs

--- src/components/Orders/UserView.js
+++ src/components/Orders/UserView.js
@@ -1,9 +1,13 @@
 import { Container, Typography, Stack } from '@mui/material';
 import OrderCard from './OrderCard';
+import OrderStatusTabs from './OrderStatusTabs';
 
 const UserView = ({ ordersData }) => {
   return (
     <Container sx={{ mt: 4, mb: 4 }}>
+  
+      <OrderStatusTabs />
+  
       <Typography variant="h4" align="center" gutterBottom>
         Your Order History
       </Typography>
