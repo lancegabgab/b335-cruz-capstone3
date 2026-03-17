54f68a007fa13bc6d8ad78d7775d945e730e58d4
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-17 08:51:34 +0800

    Create OrderStatusTabs.js

---
+++ src/components/Orders/OrderStatusTabs.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
+const OrderStatusTabs = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="To Ship" />
+        <Tab label="Shipped" />
+        <Tab label="For Delivery" />
+        <Tab label="Delivered" />
+        <Tab label="Cancelled" />
+        <Tab label="Returned" />
+      </Tabs>
+    </Box>
+  );
+};
+
+export default OrderStatusTabs;
