83d452485121d2d3450da0a9352354f7b2d334dc
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-17 09:50:55 +0800

    Update UserView.js
    removed OrderStatusTabs and heading

--- src/components/Orders/UserView.js
+++ src/components/Orders/UserView.js
@@ -1,17 +1,9 @@
 import { Container, Typography, Stack } from '@mui/material';
 import OrderCard from './OrderCard';
-import OrderStatusTabs from './OrderStatusTabs';
 
 const UserView = ({ ordersData }) => {
   return (
     <Container sx={{ mt: 4, mb: 4 }}>
-  
-      <OrderStatusTabs />
-  
-      <Typography variant="h4" align="center" gutterBottom>
-        Your Order History
-      </Typography>
-
       {ordersData.length === 0 ? (
         <Typography align="center">No orders found.</Typography>
       ) : (
