a612724c1ac4cf429bd2399737da462a18e6c438
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-03-24 10:24:02 +0800

    Updated Product page
    used useApi hook

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,34 +2,35 @@ import { useEffect, useState, useContext } from 'react';
 import UserContext from '../UserContext';
 import UserView from '../components/Products/UserView';
 import AdminView from '../components/Products/AdminView';
+import useApi from '../hooks/useApi';
 
 const Products = () => {
   const { user } = useContext(UserContext);
   const [products, setProducts] = useState([]);
+  const { callApi } = useApi(''); 
 
   const fetchData = async () => {
-    const url =
-      user.isAdmin === true
-        ? `${process.env.REACT_APP_API_URL}/products/all`
-        : `${process.env.REACT_APP_API_URL}/products/`;
+    try {
+      const endpoint =
+        user.isAdmin === true ? '/products/all' : '/products';
 
-    const res = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('access')}`,
-      },
-    });
-    const data = await res.json();
-    setProducts(data || []);
+      const data = await callApi({ method: 'GET', url: endpoint });
+
+      setProducts(data || []);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [user]);
 
   return user.isAdmin ? (
     <AdminView productsData={products} fetchData={fetchData} />
   ) : (
     <UserView productsData={products} />
   );
-}
+};
+
 export default Products;
