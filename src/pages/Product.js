d06871746550f116bf8b80af8ea45c70215447bd
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-04-04 22:59:41 +0800

    Updated Product page
    rendered Filters component

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,7 +2,9 @@ import { useEffect, useState, useContext } from "react";
 import UserContext from "../UserContext";
 import UserView from "../components/Products/UserView";
 import AdminView from "../components/Products/AdminView";
+import Filters from "../components/Products/Filters";
 import useApi from "../hooks/useApi";
+import { Typography } from "@mui/material";
 
 const Products = () => {
   const { user } = useContext(UserContext);
@@ -22,10 +24,18 @@ const Products = () => {
   if (loading) return <p>Loading products...</p>;
   if (error) return <p style={{ color: "red" }}>{error}</p>;
 
-  return user?.isAdmin ? (
-    <AdminView productsData={products} fetchData={fetchData} />
-  ) : (
-    <UserView productsData={products} />
+  return (
+    <>
+      <Typography variant="h4" align="center" gutterBottom>
+        {user?.isAdmin ? "Admin Dashboard" : ""}
+      </Typography>
+      <Filters />
+      {user?.isAdmin ? (
+        <AdminView productsData={products} fetchData={fetchData} />
+      ) : (
+        <UserView productsData={products} />
+      )}
+    </>
   );
 };
 
