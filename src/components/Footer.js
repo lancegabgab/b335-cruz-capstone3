212ef2603473371aac2cdecc403221788667dd99
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-04-14 08:27:45 +0800

    Updated Footer.js
    added textAlign center

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,6 +13,7 @@ const Footer = () => {
         px: { xs: 2, md: 8 },
         mt: 8,
         borderTop: "1px solid #e0e0e0",
+        textAlign: "center",
       }}
     >
       <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
